bed8411dc2c6ecfc208960f213929af1d7a676d4
MariannaBazzani <merionbaz@gmail.com>
2024-01-23 17:30:01 +0100

    Aggiungere i file di progetto.

---
+++ Es Rettangolo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Rettangolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rettangolo rettangolo = new Rettangolo();
+            Console.WriteLine("Inserisci base: ");
+            rettangolo.Base = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci altezza: ");
+            rettangolo.Altezza = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area : " + rettangolo.Area());
+            Console.WriteLine("Perimetro: " + rettangolo.Perimetro());
+
+            Rettangolo quadrato = new Rettangolo();
+            Console.WriteLine("Inserisci base: ");
+            quadrato.Base = quadrato.Altezza = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: " + quadrato.Area());
+            Console.WriteLine("Perimetro: " + quadrato.Perimetro());
+        }
+    }
+}
---
+++ Es Rettangolo/Rettangolo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Rettangolo
+{
+    internal class Rettangolo
+    {
+        public int Altezza { get; set; }
+        public int Base { get; set; }
+
+        public double Area()
+        {
+            return Base * Altezza;
+        }
+
+        public int Perimetro()
+        {
+            return 2 * (Base + Altezza);
+        }
+    }
+}
